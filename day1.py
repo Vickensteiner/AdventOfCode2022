885b9c14ce7088e932d203b0dc644b5f8e107e1a
Vince John <victorjmarton.vjm@gmail.com>
2022-12-01 11:41:26 -0500

    day 1

---
+++ day1.py
@@ -0,0 +1,32 @@
+with open("day1input.txt") as infile:
+    highest_cals = 0
+    current_cals = 0
+    for line in infile.readlines():
+        if(line.strip() == ""):
+            current_cals = 0
+        else:
+            current_cals = current_cals + int(line)
+            if(current_cals > highest_cals):
+                highest_cals = current_cals
+    print(highest_cals)
+# End Part 1
+
+#%%
+
+with open("day1input.txt") as infile:
+    highest_cals = [0, 0, 0]    # Rank 1, 2, 3 in cals
+    current_cals = 0
+    for line in infile.readlines():
+        if(line.strip() == ""):
+            if(current_cals > highest_cals[2]):
+                highest_cals[2] = current_cals
+            if(current_cals > highest_cals[1]):
+                highest_cals[2] = highest_cals[1]
+                highest_cals[1] = current_cals
+            if(current_cals > highest_cals[0]):
+                highest_cals[1] = highest_cals[0]
+                highest_cals[0] = current_cals
+            current_cals = 0
+        else:
+            current_cals = current_cals + int(line)
+    print(highest_cals[0] + highest_cals[1] + highest_cals[2])
